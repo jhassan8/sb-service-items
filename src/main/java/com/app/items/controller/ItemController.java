1c4862a4f348a732d37dd1dc1d66fa30ac8dd22d
Jocs <jhassan8@outlook.com>
2019-09-21 14:55:02 -0300

    Error tolerance and latency with Hystrix

--- src/main/java/com/app/items/controller/ItemController.java
+++ src/main/java/com/app/items/controller/ItemController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.items.models.Item;
+import com.app.items.models.Product;
 import com.app.items.models.service.IItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
@@ -27,9 +29,22 @@ public class ItemController {
 		return iItemService.findAll();
 	}
 	
+	@HystrixCommand(fallbackMethod = "alternMethod")
 	@GetMapping("item/{id}/count/{count}")
 	public Item item(@PathVariable Long id, @PathVariable Integer count) {
 		return iItemService.findById(id, count);
 	}
 
+	public Item alternMethod(Long id, Integer count) {
+		Item item = new Item();
+		Product product =  new Product();
+		
+		item.setCount(count);
+		product.setId(id);
+		product.setName("altern name");
+		product.setPrice(500.00);
+		item.setProduct(product);
+		
+		return item;
+	}
 }
